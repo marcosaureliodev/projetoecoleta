f21ff7f264cab572b41ddb336458dbf66923a5dc
MARCOS AUÉLIO ALVES DOS REIS <marcosaurlio89@hotmail.com>
2020-06-12 10:20:06 -0300

    Iniciando javascript

---
+++ scripts/intro.js
@@ -0,0 +1,71 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// let myvar ="He"
+// const myconst = "He"
+
+// document.write(myvar + myconst)
+
+//string
+//const s1 = "Isso é uma string"
+//const s2 = "Isso também é uma string"
+//const s3 = "Isso é uma string também"
+
+//document.write(s3)
+
+//number
+//const n1 = 1
+//const n2 = 12
+//document.write(n1+n2)
+
+//boolean - true ou false
+//const bTrue = true
+//const bFalse = false
+//document.write(bTrue)
+
+//objeto possue propriedades e funcioanlidade
+//const pessoa {
+//altura: "1,80m",
+//idade: 24,
+//solteiro: true
+//correr(){
+//  document.write("executar uma grade logica de correr")
+//}
+//}
+
+// pessoa.correr()
+
+// Array -  Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+// registrar função
+// function sayMyname(name) {
+//  document.write(name)
+// }
+
+// executar
+// sayMyname("Marcos")
+// sayMyname("Elayne")
+// sayMyname("M.Filho")
+
+// condicionais
+
+// const notaFinal = 7
+
+// if(notaFinal < 5 ) {
+//  document.write("Reprovado")
+//} else {
+//  document.write("Aprovado")
+//}
+
+// loop repetições
+// for (i = 0; i < 10; i++) {
+//  document.write(`<p>${i}</p>`)
+// }
+
+
