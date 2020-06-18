33f2a2adba318ec55d0cd9c279444bb6089493e4
MARCOS AUÉLIO ALVES DOS REIS <marcosaurlio89@hotmail.com>
2020-06-17 22:44:55 -0300

    Intens de coleta

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -28,7 +28,7 @@ function getCities(event) {
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
-  citySelect.innerHTML = "<option value>Slecione a Cidade"
+  citySelect.innerHTML = "<option value>Selecione a Cidade"
   citySelect.disabled = true
 
   fetch(url)
@@ -51,3 +51,51 @@ document
 
   //FUNÇÃO PARA SELEÇÃO ITENS DE COLETA
   //pegar todos os li´s
+
+  const itemsToCollect =  document.querySelectorAll(".items-grid li")
+
+  for (const item of itemsToCollect){
+    item.addEventListener("click",handleSelectedItem)
+  }
+
+  const collectedItems = document.querySelector("input[name=items]")
+
+  let selectedItems = []
+
+  function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    //adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+    
+    const itemId = itemLi.dataset.id
+
+    
+
+    //verificaf se exitem itens selecionados, se sim
+    //pegar os itens selecionados
+
+    const alreadySelected = selectedItems.findIndex( item => {
+      const itemFound = item == itemId // isso será true ou false
+      return itemFound
+    })
+
+    //se já tiver selecionado tirar da seleção
+    if( alreadySelected >= 0) {
+      // tirar da seleção
+      const filteredItems = selectedItems.filter( item => {
+        const itemIsDifferent = item !=itemId //false
+        return itemIsDifferent
+      })
+
+      selectedItems = filteredItems
+    } else {
+      //se não estiver selecionado
+      //adicionar à seleção
+      selectedItems.push(itemId)
+    }
+
+    //atualizar o campo escondido com os utens selecionados
+    collectedItems.value = selectedItems
+
+  }
