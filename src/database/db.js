5173fffe1903fb981ceae913ddc6c35e5cd29c99
MARCOS AUÉLIO ALVES DOS REIS <marcosaurlio89@hotmail.com>
2020-07-05 02:34:39 -0300

    Back end

--- src/database/db.js
+++ src/database/db.js
@@ -7,46 +7,46 @@ const db = new sqlite3.Database("./src/database/database.db")
 module.exports = db //module.export é usada para exporta o objeto db
 
 // utilizar o objeto de banco de dados, para nossa operações
-db.serialize(() => {
+//db.serialize(() => {
 
-  //Com comandos SQL eu irei:
+//   //Com comandos SQL eu irei:
 
-  // 1 Criar uma tabela
-  db.run(`
-          CREATE TABLE IF NOT EXISTS places (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image TEXT,
-            name TEXT,
-            address TEXT,
-            address2 TEXT,
-            state TEXT,
-            city TEXT,
-            items TEXT
-      );
-  `)
+//   // 1 Criar uma tabela
+//   db.run(`
+//           CREATE TABLE IF NOT EXISTS places (
+//             id INTEGER PRIMARY KEY AUTOINCREMENT,
+//             image TEXT,
+//             name TEXT,
+//             address TEXT,
+//             address2 TEXT,
+//             state TEXT,
+//             city TEXT,
+//             items TEXT
+//       );
+//   `)
 
-  // 2 Inserir dados na tabela
-  const query = `
-          INSERT INTO places (
-            image,
-            name,
-            address,
-            address2,
-            state,
-            city,
-            items
-          ) VALUES (?,?,?,?,?,?,?);
-    `
+//   // 2 Inserir dados na tabela
+//   const query = `
+//           INSERT INTO places (
+//             image,
+//             name,
+//             address,
+//             address2,
+//             state,
+//             city,
+//             items
+//           ) VALUES (?,?,?,?,?,?,?);
+//     `
 
-  const values = [
-    "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-    "Papersider",
-    "Guilherme Gemballa, Jardim América",
-    "Número 260",
-    "Santa Catarina",
-    "Rio do Sul",
-    "Papeis e Papelão"
-  ]
+//   const values = [
+//     "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+//     "Papersider",
+//     "Guilherme Gemballa, Jardim América",
+//     "Número 260",
+//     "Santa Catarina",
+//     "Rio do Sul",
+//     "Papeis e Papelão"
+//   ]
 
   // function afterInsertData(err) {
   //     if(err) {
@@ -71,7 +71,7 @@ db.serialize(() => {
 
 
   // 4 Deletar um dado na tabela
-  // db.run(`DELETE FROM PLACES WHERE id = ?`, [10], function(err) {
+  // db.run(`DELETE FROM PLACES WHERE id = ?`, [13], function(err) {
   //     if(err) {
   //       return console.log(err)
   //     }
@@ -79,7 +79,7 @@ db.serialize(() => {
   //     console.log("Registro deletado com sucesso!")
   // })
 
-})
+//})
 
 /* 
 Comando para execução do banco de dados 
--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ const db = require("./database/db")
 // Configurar pasta publica
 server.use(express.static("public"))
 
+// habilitar o uso do req body na nossa aplicação
+server.use(express.urlencoded({ extended: true }))
 
 // utilizando template engine
 const nunjucks = require("nunjucks")
@@ -17,27 +19,80 @@ nunjucks.configure("src/views", {
 })
 
 
-// configurar caminhos da minha aplicação
+// configurar caminhos (rotas) da minha aplicação
 // página inicial
 // req: Requisição
 // res: Resposta
 server.get("/", (req, res) =>{
-  return res.render("index.html", {title: "Um tíltulo"})
+  return res.render("index.html", {title: "Um título"})
 })
+
+
 
 server.get("/create-point", (req, res) =>{
 
   // req.query: Query Strings da nossa url
-  console.log(req.query)
+  // console.log(req.query)
   
   return res.render("create-point.html")
 })
 
-server.get("/search", (req, res) =>{
+server.post("/savepoint", (req, res) => {
+  // req.body: O corpo do formulário
+  // console.log(req.body)
+
+  // inserir dados no banco de dados
+  const query = `
+          INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+          ) VALUES (?,?,?,?,?,?,?);
+    `
+
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ]
+
+  function afterInsertData(err) {
+      if(err) {
+         console.log(err)
+         return res.send("Erro no cadastro!")
+      }
+
+      console.log("Cadastro com sucesso")
+      console.log(this)
+
+      return res.render("create-point.html", {saved: true})
+  }
+
+  db.run(query, values, afterInsertData) //db.run usado para inserir dados na tabela
+
+  
+})
+
+server.get("/search", (req, res) => {
+
+  const search = req.query.search
+
+  if(search == "") {
+    //pesquisa vazia
+    return res.render("search-results.html", { total: 0})
+  }
 
   // Pegar os dados no banco de dados
   // Consultar dados na tabela
-  db.all(`SELECT * FROM places`, function(err, rows) {
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
       if(err) {
         return console.log(err)
       }
